7f21b9eaa40d8fa89c911cfcdcee4c0ccbbbfcbd
rubo92i <ruben.manukyan@appish.co.uk>
2020-09-10 22:57:23 +0400

    init

--- src/main/java/am/basic/notificator/crm/config/RemoteConfig.java
+++ src/main/java/am/basic/notificator/crm/config/RemoteConfig.java
@@ -5,7 +5,6 @@ import am.basic.notificator.crm.service.CrmService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.remoting.caucho.HessianProxyFactoryBean;
 import org.springframework.remoting.caucho.HessianServiceExporter;
 import org.springframework.remoting.support.RemoteExporter;
 
